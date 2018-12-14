69452f672912acc8e0c2c56ae15dd29cf1d9a8c7
Microsoft <Microsoft@192.168.1.23>
2018-12-14 18:03:36 +0200

    simple chat client

---
+++ src/pkg3/pkg4server/Main.java
@@ -0,0 +1,15 @@
+
+
+package pkg3.pkg4server;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
